341396ae2c6839430c9d78ae691dc760d753243f
Sam <sam@appletekiMacBook-Pro.local>
2014-01-08 14:11:16 +0800

    First Commit

---
+++ BaseLibs/BaseLibs/BaseLibs.h
@@ -0,0 +1,27 @@
+//
+//  BaseLibs.h
+//  BaseLibs
+//
+//  Created by Sam on 14-1-5.
+//  Copyright (c) 2014年 Sam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ASIHTTPRequest.h"
+#import "ASINetworkQueue.h"
+#import "ASIDownloadCache.h"
+#import "ASIFormDataRequest.h"
+#import "ASIAuthenticationDialog.h"
+
+#import "BMapKit.h"
+
+#import "MBProgressHUD.h"
+
+#import "JSONKit.h"
+
+@interface BaseLibs : NSObject
+{
+    
+}
+@end
---
+++ BaseLibs/BaseLibs/BaseLibs.mm
@@ -0,0 +1,13 @@
+//
+//  BaseLibs.m
+//  BaseLibs
+//
+//  Created by Sam on 14-1-5.
+//  Copyright (c) 2014年 Sam. All rights reserved.
+//
+
+#import "BaseLibs.h"
+
+@implementation BaseLibs
+
+@end
